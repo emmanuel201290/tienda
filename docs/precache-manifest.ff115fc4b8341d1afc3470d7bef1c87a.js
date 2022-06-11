473227d5dce53e6eb6e3d6ee160d78b68d397e3f
emmanuel <enmanuelmartinez89@yahoo.es>
2022-06-10 23:04:55 -0600

    mod05

---
+++ docs/precache-manifest.ff115fc4b8341d1afc3470d7bef1c87a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6753658af3b58ebfe637",
+    "url": "/static/css/main.039f0010.chunk.css"
+  },
+  {
+    "revision": "6753658af3b58ebfe637",
+    "url": "/static/js/main.6753658a.chunk.js"
+  },
+  {
+    "revision": "657b04e9845ef21180ea",
+    "url": "/static/js/1.657b04e9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0f9de1e234552271e62e10f05c2e1c80",
+    "url": "/static/media/chicasCompras.0f9de1e2.png"
+  },
+  {
+    "revision": "83519c5a922a8d5e076d3663fa8d2805",
+    "url": "/static/media/NuevoLogo.83519c5a.PNG"
+  },
+  {
+    "revision": "7d22eb5dec06bd5874566bb886794c9e",
+    "url": "/index.html"
+  }
+];
